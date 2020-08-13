fef701073ac4fbcc13edc30ece8f6e04c55bb019
zhoulihuang <17801029594@163.com>
2020-08-13 20:01:20 +0800

    1.数据库字段实体对象加入长度，最大最小值

--- src/main/java/cn/adcc/client/utils/ColumnField.java
+++ src/main/java/cn/adcc/client/utils/ColumnField.java
@@ -1,11 +1,19 @@
 package cn.adcc.client.utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class ColumnField {
     private String name;
+    /*数据库类型信息前端不可见*/
+    @JsonIgnore
     private String type;
     private String javaType;
+    // 最大长度
+    private int length;
+    // 最大最小值范围(针对Integer类型)；
+    private int maxValue;
+    private int minValue;
     private Boolean nullAble;
 }
--- src/main/java/cn/adcc/client/utils/DbUtil.java
+++ src/main/java/cn/adcc/client/utils/DbUtil.java
@@ -15,6 +15,7 @@ public class DbUtil {
                     columnField.setName(field);
                     columnField.setType(type);
                     columnField.setJavaType(sqlTypeToJavaType(type));
+                    generateLength(columnField);
                     columnField.setNullAble("YES".equals(aNull));
                     return columnField;
                 })
@@ -29,9 +30,7 @@ public class DbUtil {
             return "String";
         /*} else if (sqlType.contains("tinyint".toUpperCase())) {
             return "Boolean";*/
-        } else if (sqlType.contains("bigint".toUpperCase())) {
-            return "Long";
-        } else if (sqlType.contains("int".toUpperCase())) {
+        }  else if (sqlType.contains("int".toUpperCase())) {
             return "Integer";
         } else if (sqlType.contains("datetime".toUpperCase())) {
             return "Date";
@@ -39,5 +38,45 @@ public class DbUtil {
             return "BigDecimal";
         }
         return "String";
+    }
+
+    private static void generateLength(ColumnField columnField) {
+        String sqlType = columnField.getType().toUpperCase();
+        String javaType = columnField.getJavaType();
+        int length = 0;
+        int maxValue = 0;
+        int minValue = 0;
+        if (javaType.equals("String")) {
+            if (sqlType.startsWith("VARCHAR")) {
+                length = Integer.valueOf(sqlType.substring(sqlType.indexOf("(") + 1, sqlType.length() - 1));
+            }
+            // char、text类型默认不限制长度(无限大)
+            columnField.setLength(length);
+        } else if (javaType.equals("Integer")) {
+            if (sqlType.startsWith("BIGINT")) {
+                // bigint类型不提示最大最小值
+                length = 19;
+            } else if (sqlType.startsWith("INT")) {
+                length = 10;
+                minValue = -2147483648;
+                maxValue = 2147483647;
+            } else if (sqlType.startsWith("SMALLINT")) {
+                length = 5;
+                minValue = -32768;
+                maxValue = 32767;
+            } else if (sqlType.startsWith("TINYINT")) {
+                length = 3;
+                minValue = -128;
+                maxValue = 127;
+            }
+            if (sqlType.contains("UNSIGNED")) {
+                minValue = 0;
+                maxValue = maxValue * 2 + 1;
+            }
+            // bigint 不提示范围值，其他未知int类型不限制长度
+            columnField.setLength(length);
+            columnField.setMinValue(minValue);
+            columnField.setMaxValue(maxValue);
+        }
     }
 }
